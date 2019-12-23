5c350f12c309c871f72a9bfdf0c15bcaf86ac360
Vio3301 <51275082+Vio3301@users.noreply.github.com>
2019-12-23 16:04:52 +0200

    Add files via upload

---
+++ Project Simple/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    $('.slide').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        nextArrow: 'false',
+        autoplay: true,
+        autoplaySpeed: 1500,
+        touchMove: 'false',
+      });
+      
+
+});
+
+
